fab9430ae424688730d18498990d21435f242685
AndreyMachula <amcz2020@gmail.com>
2022-07-26 19:08:57 +0200

    refactoring with React hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,37 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { PropTypes } from 'prop-types';
 
 import styles from './Modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+const Modal = ({ onCloseModal, children }) => {
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onCloseModal();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    window.addEventListener('keydown', handleKeyDown);
+    console.log();
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [onCloseModal]);
 
-  handleKeyDown = event => {
-    if(event.code === 'Escape') {
-      this.props.onCloseModal();
-    } 
+  const handleOverlayClick = event => {
+    if (event.currentTarget === event.target) {
+      onCloseModal();
+    }
   };
 
-  handleOverlayClick = event => {
-    if(event.currentTarget === event.target) {
-      this.props.onCloseModal();
-    } 
-  };
-
-  render() {
-    return (
-      <div className={styles.Overlay} onClick={this.handleOverlayClick}>
-        <div className={styles.Modal}>
-        {this.props.children}
-          </div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.Overlay} onClick={handleOverlayClick}>
+      <div className={styles.Modal}>{children}</div>
+    </div>
+  );
+};
 
 Modal.propTypes = {
   onCloseModal: PropTypes.func.isRequired,
   children: PropTypes.object.isRequired,
 };
 
-export default Modal
+export default Modal;
